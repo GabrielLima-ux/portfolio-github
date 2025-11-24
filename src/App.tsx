3f563077b783971c12f716b5c91c61efc9b7124f
Gabriel <gabrielplima0098@gmail.com>
2025-11-24 20:43:07 -0300

    Construindo a estrutura e a seção sobre mim

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import Teste from './Teste'
-import EstiloGLobal from './styles'
+import Sidebar from './containers/Sidebar'
+import Projetos from './containers/Sidebar/Projetos'
+import Sobre from './containers/Sidebar/Sobre'
+import EstiloGLobal, { Container } from './styles'
 
 function App() {
   return (
     <>
       <EstiloGLobal />
-      <Teste />
+      <Container>
+        <Sidebar />
+        <main>
+          <Sobre />
+          <Projetos />
+        </main>
+      </Container>
     </>
   )
 }
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/GabrielLima-ux.png" alt="Gabriel Lima" />
+    <Titulo fontSize={20}>Gabriel Lima</Titulo>
+  </aside>
+)
+
+export default Sidebar
